50648f48c5c332ae22e7e24716b60e293981c1a5
spenser6131 <brinkman.spenser@gmail.com>
2021-01-04 19:51:45 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+  let result = []
+  for (let i = 0; i < array.length; i++ ) {
+    result.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)  
+  }
+  return result
+}
+
+function countDown(number) {
+  while (number > -1) {
+    console.log(number--)
+  }
+}
